29ba30dbbdd8bbb4da8d1186e122ef6ce093a0b0
Arthur Dexter <adexter@dexman.net>
2020-08-07 09:06:18 -0500

    Downgrade to swift-tools-version:5.2.0
    * Don't want to wait for the next Xcode to be in production
    * Drop the tests that depend on resources for now

--- Package.swift
+++ Package.swift
@@ -1,32 +1,17 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.2.0
 
 import PackageDescription
 
 let package = Package(
     name: "TrailforksKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "TrailforksKit",
             targets: ["TrailforksKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TrailforksKit",
-            dependencies: []),
-        .testTarget(
-            name: "TrailforksKitTests",
-            dependencies: ["TrailforksKit"],
-            resources: [
-                .process("regions_detailed.json"),
-                .process("regions_error.json"),
-            ]),
+            dependencies: [])
     ]
 )
