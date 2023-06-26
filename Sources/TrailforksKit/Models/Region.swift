5a45db72519fa091987fcdf1b45092a744e88a59
Arthur Dexter <adexter@dexman.net>
2023-06-26 13:17:59 -0500

    update nullability of some Region fields

--- Sources/TrailforksKit/Models/Region.swift
+++ Sources/TrailforksKit/Models/Region.swift
@@ -12,46 +12,45 @@ import Foundation
 
 public struct Region: Decodable {
 
-    public let rid: String? // "rid": "35629"
-    public let alias: String? // "alias": "arbor-hills-nature-preserve"
-    public let title: String? // "title": "Arbor Hills Nature Preserve Off Road Bike Trail"
+    public let rid: String // "rid": "35629"
+    public let alias: String // "alias": "arbor-hills-nature-preserve"
+    public let title: String // "title": "Arbor Hills Nature Preserve Off Road Bike Trail"
     public let description: String? // "description": "[B]Trail Steward:…"
     public let searchTerms: String? // "search_terms": "DORBA Trails"
-    public let isRidingArea: Bool? // "isridingarea": "0"
 
-    public let latitude: Double? // "latitude": "33.046090"
-    public let longitude: Double? // "longitude": "-96.849163"
+    public let latitude: Double // "latitude": "33.046090"
+    public let longitude: Double // "longitude": "-96.849163"
     public let countryTitle: String? // "country_title": "United States"
     public let provinceTitle: String? // "prov_title": "Texas"
     public let cityTitle: String? // "city_title": "Plano"
 
-    public let condition: Condition? // condition": "0"
-    public let status: Status? // "status": "1"
-    public let statusTimestamp: Date? // "status_ts": "1594560530"
+    public let condition: Condition // condition": "0"
+    public let status: Status // "status": "1"
+    public let statusTimestamp: Date // "status_ts": "1594560530"
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        rid = try container.decodeIfPresent(String.self, forKey: .rid)
-        alias = try container.decodeIfPresent(String.self, forKey: .alias)
-        title = try container.decodeIfPresent(String.self, forKey: .title)
+        rid = try container.decode(String.self, forKey: .rid)
+        alias = try container.decode(String.self, forKey: .alias)
+        title = try container.decode(String.self, forKey: .title)
         description = try container.decodeIfPresent(String.self, forKey: .description)
         searchTerms = try container.decodeIfPresent(String.self, forKey: .searchTerms)
-        isRidingArea = try container.decodeIfPresent(String.self, forKey: .isRidingArea) == "1"
 
-        latitude = try container.decodeFromStringIfPresent(Double.self, forKey: .latitude)
-        longitude = try container.decodeFromStringIfPresent(Double.self, forKey: .longitude)
+        latitude = try container.decodeFromString(Double.self, forKey: .latitude)
+        longitude = try container.decodeFromString(Double.self, forKey: .longitude)
         countryTitle = try container.decodeIfPresent(String.self, forKey: .countryTitle)
         provinceTitle = try container.decodeIfPresent(String.self, forKey: .provinceTitle)
         cityTitle = try container.decodeIfPresent(String.self, forKey: .cityTitle)
 
-        condition = try container.decodeIfPresent(Condition.self, forKey: .condition)
-        status = try container.decodeIfPresent(Status.self, forKey: .status)
+        condition = try container.decode(Condition.self, forKey: .condition)
+        status = try container.decode(Status.self, forKey: .status)
 
-        let statusTimestampValue = try container.decodeFromStringIfPresent(
+        let statusTimestampValue = try container.decodeFromString(
             TimeInterval.self,
-            forKey: .statusTimestamp)
-        statusTimestamp = statusTimestampValue.map(Date.init(timeIntervalSince1970:))
+            forKey: .statusTimestamp
+        )
+        statusTimestamp = Date(timeIntervalSince1970: statusTimestampValue)
     }
 
     private enum CodingKeys: String, CodingKey {
